78f67f5eb71b9724874e3c7a8194e4130843e81d
Steve Robbins <steve@steverobbins.com>
2015-10-31 15:48:52 -0700

    Adding some tests

---
+++ test/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Magedownload CLI
+ *
+ * PHP version 5
+ *
+ * @category  MageDownload
+ * @package   MageDownload
+ * @author    Steve Robbins <steve@steverobbins.com>
+ * @copyright 2015 Steve Robbins
+ * @license   http://creativecommons.org/licenses/by/4.0/ CC BY 4.0
+ * @link      https://github.com/steverobbins/magedownload-cli
+ */
+
+/** @var \Composer\Autoload\ClassLoader */
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->setUseIncludePath(true);
