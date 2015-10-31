739402de40aa3d58acb994e6185d3f620ed22f48
Steve Robbins <steve@steverobbins.com>
2015-10-31 16:00:20 -0700

    Adding list command test

---
+++ src/MageDownload/Command/PHPUnit/TestCase.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Magedownload CLI
+ *
+ * PHP version 5
+ *
+ * @category  MageDownload
+ * @package   MageDownload
+ * @author    Steve Robbins <steve@steverobbins.com>
+ * @copyright 2015 Steve Robbins
+ * @license   http://creativecommons.org/licenses/by/4.0/ CC BY 4.0
+ * @link      https://github.com/steverobbins/magedownload-cli
+ */
+
+namespace MageDownload\Command\PHPUnit;
+
+use MageDownload\Command\ConfigureCommand;
+use MageDownload\Command\FileCommand;
+use MageDownload\Command\InfoCommand;
+use Symfony\Component\Console\Application;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Project test case
+ */
+class TestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Set up the application
+     *
+     * @return Application
+     */
+    public function getApplication()
+    {
+        $app = new Application;
+        $app->add(new ConfigureCommand);
+        $app->add(new FileCommand);
+        $app->add(new InfoCommand);
+        return $app;
+    }
+}
