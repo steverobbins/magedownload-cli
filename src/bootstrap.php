840cb2b5bd159b015dd4b6790c42aad43246a422
Steve Robbins <steve@steverobbins.com>
2015-11-03 11:36:32 -0800

    Incrementing version number

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -19,7 +19,7 @@ use MageDownload\Command\FileCommand;
 use MageDownload\Command\InfoCommand;
 use Symfony\Component\Console\Application;
 
-$app = new Application('Magedownload CLI', '1.2.1');
+$app = new Application('Magedownload CLI', '1.2.2');
 
 $app->add(new ConfigureCommand);
 $app->add(new FileCommand);
