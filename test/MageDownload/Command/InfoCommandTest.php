a2468c50499bac3c270a56cd831409a960c82b3e
Steve Robbins <steve@steverobbins.com>
2015-11-08 13:33:29 -0800

    Adding info and file command tests

---
+++ test/MageDownload/Command/InfoCommandTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Magedownload CLI
+ *
+ * PHP version 5
+ *
+ * @category  MageDownload
+ * @package   MageDownload
+ * @author    Steve Robbins <steve@steverobbins.com>
+ * @copyright 2015 Steve Robbins
+ * @license   http://creativecommons.org/licenses/by/4.0/ CC BY 4.0
+ * @link      https://github.com/steverobbins/magedownload-cli
+ */
+
+namespace MageDownload\Test\Command;
+
+use MageDownload\Command\PHPUnit\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Test the info command
+ */
+class InfoCommandTest extends TestCase
+{
+    /**
+     * Test the files action
+     *
+     * @return void
+     */
+    public function testFilesAction()
+    {
+        $command       = $this->getApplication()->find('info');
+        $commandTester = new CommandTester($command);
+        $result        = $commandTester->execute([
+            'command' => 'info',
+            'action'  => 'files',
+            '--id'    => $_SERVER['MAGENTO_ID'],
+            '--token' => $_SERVER['MAGENTO_TOKEN'],
+        ]);
+        $this->assertEquals(0, $result);
+        $this->assertContains('File Name', $commandTester->getDisplay());
+    }
+
+    /**
+     * Test the files action
+     *
+     * @return void
+     */
+    public function testVersionsAction()
+    {
+        $command       = $this->getApplication()->find('info');
+        $commandTester = new CommandTester($command);
+        $result        = $commandTester->execute([
+            'command' => 'info',
+            'action'  => 'versions',
+            '--id'    => $_SERVER['MAGENTO_ID'],
+            '--token' => $_SERVER['MAGENTO_TOKEN'],
+        ]);
+        $this->assertEquals(0, $result);
+        $this->assertContains('CE Versions', $commandTester->getDisplay());
+        $this->assertContains('EE Versions', $commandTester->getDisplay());
+    }
+}
